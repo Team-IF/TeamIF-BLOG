ded43687523ce5a7ecf684b2100ca6dcdc1dc8ff
Coder Iro <doohee006@gmail.com>
2020-06-12 20:13:58 +0900

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,13 +1,13 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://www.jamify.org`,
+    siteUrl: `https://blog.teamif.io`,
 
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Team IF`,
 
     // This allows an site description for meta data for pages.
     siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
@@ -17,7 +17,7 @@ module.exports = {
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Team IF`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
